b49937194fe1f2169b6f2a22bf6464441eed0475
adaglfyl <159255725+adaglfyl@users.noreply.github.com>
2024-03-21 21:38:14 +0000

    Adding lightbox controls

--- what-nah-javascript.js
+++ what-nah-javascript.js
@@ -2,20 +2,49 @@ const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
+const close = document.createElement('a');
+close.id = 'close';
+close.innerText = "close x"
+
+const next = document.createElement('a');
+next.id = 'next';
+next.innerText = 'next';
+
+const prev = document.createElement('a');
+prev.id = 'prev';
+prev.innerText = 'prev';
+
 const images = document.querySelectorAll('.gallery-img');
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active');
         const img = document.createElement('img');
         img.src = image.src;
+      
         while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild)
         }
-        lightbox.appendChild(img)
+        lightbox.appendChild(close);
+        lightbox.appendChild(prev);
+        lightbox.appendChild(img);
+        lightbox.appendChild(next);
     })
 });
 
-lightbox.addEventListener('click', e => {
-   // if (e.target !== e.currentTarget) return // <enable for outside box clicks only
+close.addEventListener('click', e => {
     lightbox.classList.remove('active')
+});
+// Add functionality to next and prev buttons
+let ImageIndex = 0;
+next.addEventListener('click', e => {
+      ImageIndex = (ImageIndex + 1) % images.length;
+    const nextImage = images[ImageIndex];
+  const lightboxImage = document.querySelector('#lightbox img'); 
+  lightboxImage.src = nextImage.src;
+})
+prev.addEventListener('click', e => {
+      ImageIndex = (ImageIndex - 1 + images.length) % images.length;
+    const prevImage = images[ImageIndex];
+  const lightboxImage = document.querySelector('#lightbox img'); 
+  lightboxImage.src = prevImage.src;
 })
