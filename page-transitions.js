6a4e84218e989d83d293e34a67c1938bc00908cc
adaglfyl <159255725+adaglfyl@users.noreply.github.com>
2024-04-16 12:17:07 +0100

    updating formatting & cache issue

--- page-transitions.js
+++ page-transitions.js
@@ -1,30 +1,36 @@
 //Transitions between pages
 window.onload = () => {
-    const anchors = document.querySelectorAll('.nav-button');
-    const transition_el = document.querySelector('.transition');
-    const loading = document.querySelector('.dots');
-  
-    setTimeout(() => {
-      transition_el.classList.remove('is-active');
-      loading.classList.remove('is-active');
-    }, 250);
-  
-    for (let i = 0; i < anchors.length; i++) {
-      const anchor = anchors[i];
-  
-      anchor.addEventListener('click', e => {
-        e.preventDefault();
-        let main = document.querySelector('main');
-        console.log(main);
-        main.classList.add('fadeout-side');
-        let target = e.target.href;
-  
-        transition_el.classList.add('is-active');
-        loading.classList.add('is-active');
-  
-        setTimeout(() => {
-          window.location.href = target;
-        }, 1000);
-      })
-    }
+  const anchors = document.querySelectorAll('.nav-button');
+  const transition_el = document.querySelector('.transition');
+  const loading = document.querySelector('.dots');
+
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+    loading.classList.remove('is-active');
+  }, 250);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let main = document.querySelector('main');
+      console.log(main);
+      main.classList.add('fadeout-side');
+      let target = e.target.href;
+
+      transition_el.classList.add('is-active');
+      loading.classList.add('is-active');
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 1000);
+    })
+  }
 }
+//Force reload is bfcache is used
+window.onpageshow = function (event) {
+  if (event.persisted) {
+    window.location.reload()
+  }
+};
--- what-nah-javascript.js
+++ what-nah-javascript.js
@@ -18,7 +18,7 @@ function loopWren() {
     // Show the current image
     wrenImages[currentIndexWren].classList.add('show');
     // Increment index for next image
-                currentIndexWren = (currentIndexWren + 1) % wrenImages.length;
+    currentIndexWren = (currentIndexWren + 1) % wrenImages.length;
     // Call loopImages function recursively after a delay
     setTimeout(loopWren, 3000);
 }
@@ -34,7 +34,7 @@ function loopLewis() {
     // Show the current image
     lewisImages[currentIndexLewis].classList.add('show');
     // Increment index for next image
-                currentIndexLewis = (currentIndexLewis + 1) % lewisImages.length;
+    currentIndexLewis = (currentIndexLewis + 1) % lewisImages.length;
     // Call loopImages function recursively after a delay
     setTimeout(loopLewis, 3750);
 }
@@ -50,7 +50,7 @@ function loopAda() {
     // Show the current image
     adaImages[currentIndexAda].classList.add('show');
     // Increment index for next image
-                currentIndexAda = (currentIndexAda + 1) % adaImages.length;
+    currentIndexAda = (currentIndexAda + 1) % adaImages.length;
     // Call loopImages function recursively after a delay
     setTimeout(loopAda, 4500);
 }
@@ -66,7 +66,7 @@ function loopMark() {
     // Show the current image
     markImages[currentIndexMark].classList.add('show');
     // Increment index for next image
-                currentIndexMark = (currentIndexMark + 1) % markImages.length;
+    currentIndexMark = (currentIndexMark + 1) % markImages.length;
     // Call loopImages function recursively after a delay
     setTimeout(loopMark, 5250);
 }
@@ -95,9 +95,12 @@ const images = document.querySelectorAll('.gallery-img');
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active');
+        lightbox.style.animation = "fadein 0.5s ease";
+        setTimeout(() => {
+            lightbox.style.animation = "none";
+        }, 400);
         const img = document.createElement('img');
         img.src = image.src;
-      
         while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild)
         }
@@ -107,25 +110,56 @@ images.forEach(image => {
         lightbox.appendChild(next);
     })
 });
+//Create lightbox control functions//
+function nextFunction() {
+    ImageIndex = (ImageIndex + 1) % images.length;
+    const nextImage = images[ImageIndex];
+    const lightboxImage = document.querySelector('#lightbox img');
+    lightboxImage.style.animation = "none";
+    lightboxImage.style.animation = "fadeout-side 0.3s linear";
+    setTimeout(() => {
+        lightboxImage.style.animation = "fadein-side 0.3s linear";
+        lightboxImage.src = nextImage.src;
+    }, 300);
+    setTimeout(() => {
+        lightboxImage.style.animation = "none";
+    }, 600);
+};
+function prevFunction() {
+    ImageIndex = (ImageIndex - 1 + images.length) % images.length;
+    const prevImage = images[ImageIndex];
+    const lightboxImage = document.querySelector('#lightbox img');
+    lightboxImage.style.animation = "none";
+    lightboxImage.style.animation = "fadeout-otherside 0.3s linear";
+    setTimeout(() => {
+        lightboxImage.style.animation = "fadein-otherside backwards 0.3s linear";
+        lightboxImage.src = prevImage.src;
+    }, 300);
+    setTimeout(() => {
+        lightboxImage.style.animation = "none";
+    }, 600);
+};
+function closeFunction() {
+    lightbox.style.animation = "fadeout 0.5s ease";
+    setTimeout(() => {
+        lightbox.classList.remove('active');
+        lightbox.style.animation = "none";
+    }, 400);
+}
 // Add functionality to close
-close.addEventListener('click', e => {
-    lightbox.classList.remove('active')
-});
+close.addEventListener('click', closeFunction);
 // Add functionality to next and prev buttons
 let ImageIndex = 0;
-next.addEventListener('click', e => {
-      ImageIndex = (ImageIndex + 1) % images.length;
-    const nextImage = images[ImageIndex];
-  const lightboxImage = document.querySelector('#lightbox img'); 
-  lightboxImage.style.animation = "fadein-side 1s linear forwards";
-  lightboxImage.src = nextImage.src;
-})
-prev.addEventListener('click', e => {
-      ImageIndex = (ImageIndex - 1 + images.length) % images.length;
-    const prevImage = images[ImageIndex];
-  const lightboxImage = document.querySelector('#lightbox img');
-  lightboxImage.classList.add('slide-right');
-  lightboxImage.src = prevImage.src;
-  lightboxImage.classList.remove('slide-right');
-})
-//Keyboard functionality for Lightbox needed
+next.addEventListener('click', nextFunction);
+prev.addEventListener('click', prevFunction);
+// Add keyboard functionality
+document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+        closeFunction();
+    } else if (e.key === 'ArrowRight') {
+        nextFunction();
+    } else if (e.key === 'ArrowLeft') {
+        prevFunction();
+    }
+});
+//add touch screen swipe functionality//
